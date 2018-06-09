a39aacb73567c6c295d7e99bfe17f03dcad71d42
yothinix <m@yothinix.com>
2018-06-09 10:27:22 +0700

    Add simple consumer :rocket:

--- python/basic_kafka/simple/consumer.py
+++ python/basic_kafka/simple/consumer.py
@@ -1,8 +1,19 @@
 from confluent_kafka import Consumer
 
-def main():
-    pass
 
+def main():
+    consumer = Consumer({
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'HuMan',
+        'default.topic.config': {
+            'auto.offset.reset': 'earliest'
+        }
+    })
+
+    consumer.subscribe(['test-topic'])
+    message = consumer.poll(1.0)
+
+    print(f"Message {message.value()}")
 
 if __name__ == '__main__':
     main()
