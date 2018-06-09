9d591dc27759639e299f7b3c979a4c3e56b57e36
yothinix <m@yothinix.com>
2018-06-09 10:34:20 +0700

    Send timestamp :rocket:

--- python/basic_kafka/simple/producer.py
+++ python/basic_kafka/simple/producer.py
@@ -1,12 +1,15 @@
 from confluent_kafka import Producer
+from datetime import datetime
 
 
 def main():
+    current_time = datetime.now().isoformat()
+
     producer = Producer({
         'bootstrap.servers': 'localhost:9092'
     })
 
-    producer.produce('test-topic', b'Hello, Kafka.')
+    producer.produce('test-topic', f'Hello, {current_time}'.encode('utf-8'))
     producer.flush()
 
 if __name__ == '__main__':
