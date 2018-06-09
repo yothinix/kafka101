bc06c849b07adca9c8655059443e0b46e29e42a0
yothinix <m@yothinix.com>
2018-06-09 12:53:43 +0700

    Disable Autocommit :rocket:

--- python/basic_kafka/simple/consumer.py
+++ python/basic_kafka/simple/consumer.py
@@ -4,7 +4,8 @@ from confluent_kafka import Consumer
 def main():
     consumer = Consumer({
         'bootstrap.servers': 'localhost:9092',
-        'group.id': 'HuMan',
+        'group.id': 'HuMan-1',
+        'enable-autocommit': False,
         'default.topic.config': {
             'auto.offset.reset': 'earliest'
         }
@@ -16,6 +17,7 @@ def main():
         message = consumer.poll(1.0)
         if message:
             print(f"Message {message.value()}")
+            consumer.commit()
 
 if __name__ == '__main__':
     main()
